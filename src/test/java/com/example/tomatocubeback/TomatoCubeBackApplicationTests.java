67562ec2d727b8bd0838921aff262cb8e5c7f614
陌离 <h2892197119@foxmail.com>
2025-07-28 15:41:31 +0800

    创建项目

---
+++ src/test/java/com/example/tomatocubeback/TomatoCubeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomatocubeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatoCubeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
